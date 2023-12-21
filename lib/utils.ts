9258e4a8dc117ef7b6660b5e1ae106ec799a914a
Howard Kao <43049909+hkao1210@users.noreply.github.com>
2023-12-21 00:47:48 -0500

    Initializing Environments part 2: Understanding how components work for ShadCN

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
  
+//allow override tailwind classes since it can combine 2 classes, but when there's a dupe, the right one takes precedence. 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }

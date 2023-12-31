0974186817ebff5a1169ab8c90da98618457e65a
Howard Kao <43049909+hkao1210@users.noreply.github.com>
2023-12-30 23:51:35 -0500

    Added server dropdown menu items

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -30,7 +30,7 @@ const ServerIdLayout = async ({
     return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-                <ServerSidebar serverId={params.serverId} /> // Add the missing import and pass the serverId prop correctly
+                <ServerSidebar serverId={params.serverId} /> 
             </div>
             <main className="h-full md:pl-60">
                 {children}

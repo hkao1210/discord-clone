ab6190ba7a9952d32fa50fec96ae02a7f3e81b1e
Howard Kao <43049909+hkao1210@users.noreply.github.com>
2023-12-31 22:17:41 -0500

    create invite link

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,6 +1,7 @@
 "use client";//This just means it's a client-side component, not a react-server component. Both are rendered on server-side.
 import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { useEffect, useState } from "react";
+import { InviteModal } from "@/components/modals/invite-modal";
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
     
@@ -15,6 +16,7 @@ export const ModalProvider = () => {
     return (
         <>
             <CreateServerModal />
+            <InviteModal />
         </>
     )
 };

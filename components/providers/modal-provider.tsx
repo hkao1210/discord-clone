c6275e6b4d7cce92fced37c02a59493e4dc97e9b
Howard Kao <43049909+hkao1210@users.noreply.github.com>
2024-01-18 23:14:49 -0500

    Edit channel name function

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -8,6 +8,7 @@ import { CreateChannelModal } from "@/components/modals/create-channel-modal";
 import { DeleteServerModal } from "@/components/modals/delete-server-modal";
 import { LeaveServerModal } from "@/components/modals/leave-server-modal";
 import { CreateServerModal } from "@/components/modals/create-server-modal";
+import { EditChannelModal } from "@/components/modals/edit-channel-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -30,6 +31,7 @@ export const ModalProvider = () => {
             <LeaveServerModal />
             <DeleteServerModal />
             <DeleteChannelModal />
+            <EditChannelModal />
         </>
     )
 };
